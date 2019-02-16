40e4d1c3e1cceee2836d0fc5ab46f463c9b166be
Cen M Galdre <galdre@galdrecraft.com>
2019-02-16 14:57:09 -0800

    publish build

---
+++ precache-manifest.ec03ff4e0a8cd29cd7042e49e9ee3fca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "f36febcc7bb7c08adc3b",
+    "url": "/portfolio/static/js/main.f36febcc.chunk.js"
+  },
+  {
+    "revision": "1015926caf23046f2ba3",
+    "url": "/portfolio/static/js/1.1015926c.chunk.js"
+  },
+  {
+    "revision": "f36febcc7bb7c08adc3b",
+    "url": "/portfolio/static/css/main.63f90ecf.chunk.css"
+  },
+  {
+    "revision": "1476c645c193d2bc419946fa73083092",
+    "url": "/portfolio/index.html"
+  }
+];
